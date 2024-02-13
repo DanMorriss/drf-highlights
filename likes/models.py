80cf29fcc33bd101bf4b1735fc4b44bbdbd81543
Dan Morriss <danielmorriss1@gmail.com>
2024-02-13 15:37:42 +0000

    Add likes and and like model

---
+++ likes/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+from highlights.models import Highlight
+
+
+class Like(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    highlight = models.ForeignKey(Highlight, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'{self.owner} {self.highlight}'
