0ad9de261cfb2fc9542abbe1760257ff4fa73bb7
Dan Morriss <danielmorriss1@gmail.com>
2024-02-14 11:06:42 +0000

    Add backend filet to comments

--- comments/views.py
+++ comments/views.py
@@ -2,12 +2,17 @@ from rest_framework import generics, permissions
 from drf_highlights.permissions import IsOwnerOrReadOnly
 from comments.models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CommentList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
+    filter_backends = [
+        DjangoFilterBackend
+    ]
+    filterset_fields = ['highlight']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
