424d2e2bee1049bfa2f361809cd27c123568a6f4
Dan Morriss <danielmorriss1@gmail.com>
2024-02-14 14:30:38 +0000

    Add likes and comments count to highlights

--- highlights/views.py
+++ highlights/views.py
@@ -1,19 +1,34 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
 from .models import Highlight
 from .serializers import HighlightSerializer
 from drf_highlights.permissions import IsOwnerOrReadOnly
 
 
 class HighlightList(generics.ListCreateAPIView):
-    queryset = Highlight.objects.all()
+    queryset = Highlight.objects.annotate(
+        comments_count=Count('comment', distinct=True),
+        likes_count=Count('like', distinct=True)
+    ).order_by('-created_on')
     serializer_class = HighlightSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'comments_count',
+        'likes_count',
+        'likes__created_at',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
 class HighlightDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Highlight.objects.all()
+    queryset = Highlight.objects.annotate(
+        comments_count=Count('comment', distinct=True),
+        likes_count=Count('like', distinct=True)
+    ).order_by('-created_on')
     serializer_class = HighlightSerializer
     permission_classes = [IsOwnerOrReadOnly]
