d8ad1b7989ec368d516f6533d8200db6effc96ba
Dan Morriss <danielmorriss1@gmail.com>
2024-02-14 14:17:07 +0000

    Add followers, following and highlights count to profiles, fix url path for detail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,35 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
 from drf_highlights.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.all()
+    # annotate adds a new field to the queryset
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__highlight', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at'
+    ]
 
 
-class ProfileDetail(generics.RetrieveAPIView):
-    queryset = Profile.objects.all()
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__highlight', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
