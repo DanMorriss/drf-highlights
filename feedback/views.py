26b5988d785c448ce9cf78da614a473a10eb9108
Dan Morriss <danielmorriss1@gmail.com>
2024-02-15 11:05:04 +0000

    Add search feature to feedback

--- feedback/views.py
+++ feedback/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from .models import Feedback
 from .serializers import FeedbackSerializer
 
@@ -7,6 +7,10 @@ class FeedbackList(generics.ListCreateAPIView):
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
     permission_classes = [permissions.IsAdminUser]
+    filter_backends = [
+        filters.SearchFilter,
+    ]
+    search_fields = ['user__username', 'content']
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
