4f3743222982a79fa03343df1de789b312bd1d32
Dan Morriss <danielmorriss1@gmail.com>
2024-02-15 11:02:50 +0000

    Add search feature to followers

--- followers/views.py
+++ followers/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from drf_highlights.permissions import IsOwnerOrReadOnly
 from .models import Follower
 from .serializers import FollowerSerializer
@@ -8,6 +8,12 @@ class FollowerList(generics.ListCreateAPIView):
     serializer_class = FollowerSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Follower.objects.all()
+    filter_backends = [
+        filters.SearchFilter,
+    ]
+    search_fields = [
+        'followed_id__username',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
