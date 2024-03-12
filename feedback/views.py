3f9e369b983416c67cd27d3061981553de3bf3ee
Dan Morriss <danielmorriss1@gmail.com>
2024-03-12 14:49:13 +0000

    Remove permissions for feedback in views.py

--- feedback/views.py
+++ feedback/views.py
@@ -4,13 +4,14 @@ from .serializers import FeedbackSerializer
 
 
 class FeedbackList(generics.ListCreateAPIView):
+    """Allows users to submit feedback"""
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
-    permission_classes = [permissions.IsAdminUser]
+
     filter_backends = [
         filters.SearchFilter,
     ]
     search_fields = ['user__username', 'content']
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    # def perform_create(self, serializer):
+    #     serializer.save(user=self.request.user)
