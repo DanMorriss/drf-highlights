4090a4a61fec481b911bb67828795ebe73de9329
Dan Morriss <danielmorriss1@gmail.com>
2024-03-12 14:44:59 +0000

    Update permission for feedbaakc view to be isAuthernticatedOrReadOnly

--- feedback/serializers.py
+++ feedback/serializers.py
@@ -3,7 +3,7 @@ from .models import Feedback
 
 
 class FeedbackSerializer(serializers.ModelSerializer):
-    # user = serializers.ReadOnlyField(source='user.username')
+    user = serializers.ReadOnlyField(source='user.username')
 
     class Meta:
         """Meta field to specify the model and fields"""
