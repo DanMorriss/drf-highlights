e6072cf4cd153c31de74025e11810387a82a5dde
Dan Morriss <danielmorriss1@gmail.com>
2024-03-12 14:39:19 +0000

    Remove readonly fireld to username in feedback serializer

--- feedback/models.py
+++ feedback/models.py
@@ -3,11 +3,13 @@ from django.contrib.auth.models import User
 
 
 class Feedback(models.Model):
+    """Model for the feedback form"""
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
+        """Order the most recent feedback first"""
         ordering = ['-created_at']
 
     def __str__(self):
--- feedback/serializers.py
+++ feedback/serializers.py
@@ -3,9 +3,10 @@ from .models import Feedback
 
 
 class FeedbackSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
+    # user = serializers.ReadOnlyField(source='user.username')
 
     class Meta:
+        """Meta field to specify the model and fields"""
         model = Feedback
         fields = [
             'id',
